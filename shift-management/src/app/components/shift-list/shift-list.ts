85f8048576240ac009d9e6f5e44c0313c02dee6c
Vignesh Balakrishnan <bvignesh34@gmail.com>
2025-06-07 15:46:31 +0530

    shift component

--- shift-management/src/app/components/shift-list/shift-list.ts
+++ shift-management/src/app/components/shift-list/shift-list.ts
@@ -1,11 +1,18 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shift-list',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './shift-list.html',
   styleUrl: './shift-list.scss'
 })
 export class ShiftList {
+  shifts: any = [{
+    type: 'morning',
+    date: new Date(),
+    assigned: 10,
+    capacity: 30
+  }]
 
 }
